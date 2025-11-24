ed3382e002e986f6ada016dc033c8ef0c96e6e6c
Joseph Pal <josephpal27@gmail.com>
2025-11-24 16:12:23 +0530

    Banner Slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,61 @@
 import '../css/Home.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+import bannerSlide1 from '/images/banners/home-banner-1.avif';
+import bannerSlide2 from '/images/banners/home-banner-2.avif';
+import logo from '../assets/images/logo/logo.avif';
 
 const Home = () => {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            {/* Banner Slider */}
+            <section className="banner">
+                <Swiper
+                    modules={[Autoplay, EffectFade]}
+                    effect="fade"
+                    fadeEffect={{ crossFade: true }}
+                    slidesPerView={1}
+                    centeredSlides={true}
+                    loop={true}
+                    grabCursor={false}
+                    spaceBetween={0}
+                    speed={1000}
+                    autoplay={{
+                        delay: 4000,
+                        disableOnInteraction: false,
+                    }}
+                >
+
+                    <SwiperSlide>
+                        <img src={bannerSlide1} alt="Banner Slide 1" loading="lazy" />
+                        <div className="slide-content">
+                            <img src={logo} alt="Logo" loading="lazy" />
+                            <h1>THE HIMALAYAN ELIXIR</h1>
+                            <div className="line"></div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={bannerSlide1} alt="Banner Slide 2" loading="lazy" />
+                        <div className="slide-content">
+                            <p>For the discerning, health-conscious consumer seeking a beverage that blends pleasure, wellness, and sophistication, Darjeeling Brew is a premium Himalayan infusion crafted from the finest leaves in the world's most iconic tea region. Unlike ordinary tea or coffee, it offers a gentle lift, an exquisite aroma, and restorative benefits making it the brew for modern, mindful living.</p>
+                            <div className="line"></div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={bannerSlide2} alt="Banner Slide 3" loading="lazy" />
+                        <div className="slide-content">
+                            <h2>The Wellness Brew for the <br /> Morden Living</h2>
+                            <div className="line"></div>
+                        </div>
+                    </SwiperSlide>
+
+                </Swiper>
+            </section>
+        </>
+    )
 }
 
 export default Home
