6f89d2dfd59c1c42a6fd6d75f530778c1d4bf188
Joseph Pal <josephpal27@gmail.com>
2025-11-24 18:14:18 +0530

    Our Products Added

--- src/components/our-products/OurProducts.jsx
+++ src/components/our-products/OurProducts.jsx
@@ -4,7 +4,11 @@ const OurProducts = () => {
   return (
     <>
       <section className="our-products">
-        
+        <div className="our-products-head">
+            <h5>Our Products</h5>
+            <span>Serving Customers Worldwide</span>
+            <p>Select from our range of The Darjeeling Brew, <br /> handpicked for the discerning consumers of today.</p>
+        </div>
       </section>
     </>
   )
