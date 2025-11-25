c3e7d91ad388c644b0cbb4a618a02d143eeaf6a0
Joseph Pal <josephpal27@gmail.com>
2025-11-25 12:37:00 +0530

    Home Page About Us content updated

--- src/components/home-about-us/HomeAboutUs.jsx
+++ src/components/home-about-us/HomeAboutUs.jsx
@@ -1,11 +1,22 @@
 import './HomeAboutUs.css';
+import aboutImage1 from '../../assets/images/about-1.avif';
+import aboutImage2 from '../../assets/images/about-2.avif';
+import aboutImage3 from '../../assets/images/about-3.avif';
 
 const HomeAboutUs = () => {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <section className="home-about-us">
+                <h6>About Us</h6>
+                <div className="about-us-image-row">
+                    <img src={aboutImage1} alt="About Image 1" loading="lazy" />
+                    <img src={aboutImage2} alt="About Image 2" loading="lazy" />
+                    <img src={aboutImage3} alt="About Image 3" loading="lazy" />
+                </div>
+                <p>With a 200 year legacy, The Darjeeling Brew is one of the largest marketers of Darjeeling tea. It is your one stop source for premium Darjeeling Tea, across all grades and qualities directly from the best estates in Darjeeling.</p>
+            </section>
+        </>
+    )
 }
 
 export default HomeAboutUs
