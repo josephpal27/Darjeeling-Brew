21dcdc57d009b9461e3bb4c0e4cb4cb8ada4ed85
Joseph Pal <josephpal27@gmail.com>
2025-11-25 16:16:10 +0530

    Cold Brewing Rituals added

--- src/components/art-of-brewing/ArtOfBrewing.jsx
+++ src/components/art-of-brewing/ArtOfBrewing.jsx
@@ -8,6 +8,11 @@ import hotBrew4 from '../../assets/images/brew-icons/hot/4.png';
 import hotBrew5 from '../../assets/images/brew-icons/hot/5.png';
 import hotBrew6 from '../../assets/images/brew-icons/hot/6.png';
 
+import coldBrew1 from '../../assets/images/brew-icons/cold/1.png';
+import coldBrew2 from '../../assets/images/brew-icons/cold/2.png';
+import coldBrew3 from '../../assets/images/brew-icons/cold/3.png';
+import coldBrew4 from '../../assets/images/brew-icons/cold/4.png';
+
 const ArtOfBrewing = () => {
     return (
         <>
@@ -36,14 +41,6 @@ const ArtOfBrewing = () => {
                                     <div className="curvy-line">
                                         <img src={curvyLine} alt="Line" loading="lazy" />
                                     </div>
-                                    {/* <div className="timeline-images">
-                                        <img src={hotBrew1} alt="Hot Brew 1" loading="lazy" />
-                                        <img src={hotBrew2} alt="Hot Brew 2" loading="lazy" />
-                                        <img src={hotBrew3} alt="Hot Brew 3" loading="lazy" />
-                                        <img src={hotBrew4} alt="Hot Brew 4" loading="lazy" />
-                                        <img src={hotBrew5} alt="Hot Brew 5" loading="lazy" />
-                                        <img src={hotBrew6} alt="Hot Brew 6" loading="lazy" />
-                                    </div> */}
                                     <div className="timeline-content">
                                         <div className="box" id="hot-box-1">
                                             <div className="box-image">
@@ -96,14 +93,65 @@ const ArtOfBrewing = () => {
                                             </div>
                                             <div className="box-content">
                                                 <span>Make It Your Own</span>
-                                                <p>Add a dash of milk and a dollop of honey if you so desire.</p>
+                                                <p>Add a dash of milk and a dollop of <br /> honey if you so desire.</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </Tab.Pane>
                             <Tab.Pane eventKey="tabTwo" className="fade">
-                                Content 2
+                                <div className="brew-timeline">
+                                    <div className="curvy-line">
+                                        <img src={curvyLine} alt="Line" loading="lazy" />
+                                    </div>
+                                    <div className="timeline-content" id="cold-timeline-content">
+                                        <div className="box" id="cold-box-1">
+                                            <div className="box-image">
+                                                <img src={coldBrew1} alt="Cold Brew 1" loading="lazy" />
+                                            </div>
+                                            <div className="box-content">
+                                                <span>Begin with Stillness</span>
+                                                <p>Choose a quiet moment. Take fresh cold water in a jug. The clearer the water, the clearer the mind.</p>
+                                            </div>
+                                        </div>
+                                        <div className="box" id="cold-box-2">
+                                            <div className="box-image">
+                                                <img src={coldBrew2} alt="Cold Brew 2" loading="lazy" />
+                                            </div>
+                                            <div className="box-content">
+                                                <span>Add The Darjeeling Brew</span>
+                                                <p>1 spoonful of The Darjeeling Brew Leaves for every 250 ml of water. Let the leaves sink slowly, like mist settling over the Darjeeling slopes.</p>
+                                            </div>
+                                        </div>
+                                        <div className="box" id="cold-box-3">
+                                            <div className="box-image">
+                                                <img src={coldBrew3} alt="Cold Brew 3" loading="lazy" />
+                                            </div>
+                                            <div className="box-content">
+                                                <span>Let Time Work Its Magic</span>
+                                                <p>Let it infuse for minimum 30 minutes upto 6-8 hours. Overnight is perfect. The flavors unfold at their own pace, becoming velvet-smooth and floral.</p>
+                                            </div>
+                                        </div>
+                                        <div className="box" id="cold-box-4">
+                                            <div className="box-image">
+                                                <img src={coldBrew4} alt="Cold Brew 4" loading="lazy" />
+                                            </div>
+                                            <div className="box-content">
+                                                <span>Pour the Golden Brew</span>
+                                                <p>Remove the leaves. Pour the golden, crystal-clear brew in a glass. Watch it glisten like morning sunlight over the Himalayas.</p>
+                                            </div>
+                                        </div>
+                                        <div className="box" id="cold-box-5">
+                                            <div className="box-image">
+                                                <img src={hotBrew4} alt="Cold Brew 5" loading="lazy" />
+                                            </div>
+                                            <div className="box-content">
+                                                <span>Taste the Cool of the Mountains</span>
+                                                <p>One sip and you meet a different side of Darjeeling: crisp, refreshing, hydrating, and full of antioxidants. A gentle lift, serenity and clarity in mind.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </Tab.Pane>
                         </Tab.Content>
                     </Tab.Container>
