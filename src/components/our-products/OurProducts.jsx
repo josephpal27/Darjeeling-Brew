4e7cbc4102bb5eb3dc3621ec395b838e86c01efb
Joseph Pal <josephpal27@gmail.com>
2025-12-02 18:14:06 +0530

    Our Products Updated

--- src/components/our-products/OurProducts.jsx
+++ src/components/our-products/OurProducts.jsx
@@ -1,12 +1,8 @@
 import './OurProducts.css';
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import productSlide1 from '../../assets/images/products-slider/product-1.avif';
-import productSlide2 from '../../assets/images/products-slider/product-2.avif';
-import productSlide3 from '../../assets/images/products-slider/product-3.avif';
-import productSlide4 from '../../assets/images/products-slider/product-4.avif';
+import productImage1 from '../../assets/images/our-products/product-1.avif';
+import productImage2 from '../../assets/images/our-products/product-2.avif';
+import productImage3 from '../../assets/images/our-products/product-3.avif';
+import productImage4 from '../../assets/images/our-products/product-4.avif';
 
 const OurProducts = () => {
   return (
@@ -17,58 +13,60 @@ const OurProducts = () => {
           <span>Serving Customers Worldwide</span>
           <p>Select from our range of The Darjeeling Brew, <br /> handpicked for the discerning consumers of today.</p>
         </div>
-        <div className="our-products-slider">
-          <Swiper
-            modules={[Navigation]}
-            slidesPerView={1}
-            centeredSlides={true}
-            loop={true}
-            grabCursor={false}
-            spaceBetween={0}
-            speed={1000}
-            navigation={{
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
-            }}
-          >
+        
+        {/* Products Container */}
+        <div className="products-container">
+          <span className="product-category">B2B</span>
 
-            <SwiperSlide id="product-slide-1">
-              <span>CONSUMER PACKS</span>
-              <img src={productSlide1} alt="Product 1" loading="lazy" />
-            </SwiperSlide>
+          {/* Box 1 */}
+          <div className="product-box" id="product-box-1">
+            <span className="product-title">HIMALAYAN ELIXIR GOLD</span>
+            <span className="product-sub-title">Available in Whole Leaf & Fannings/Flakes</span>
+            <div className="product-image">
+              <img src={productImage1} alt="Product Image" loading="lazy" />
+            </div>
+            <div className="box-foot">
+              <span>Product Essence</span>
+              <p>The rarest of rare harvests, plucked at dawn in early spring, when the Himalayan mist is still rests on the leaves, grown and produced in limited quantities.</p>
+            </div>
+          </div>
 
-            <SwiperSlide id="product-slide-2">
-              <span>HIMALAYAN ELIXIR CLASSIC</span>
-              <img src={productSlide2} alt="Product 2" loading="lazy" />
-              <div className="slide-foot">
-                <span>Product Essence</span>
-                <p>Harvested during the monsoons, offering a smooth, full-bodied taste. Everyday luxury that brings the mountain to your morning. Can be had with a dash of milk and sugar.</p>
-              </div>
-            </SwiperSlide>
+          {/* Box 2 */}
+          <div className="product-box" id="product-box-2">
+            <span className="product-title">HIMALAYAN ELIXIR RUBY</span>
+            <span className="product-sub-title">Available in Whole Leaf & Fannings/Flakes</span>
+            <div className="product-image">
+              <img src={productImage2} alt="Product Image" loading="lazy" />
+            </div>
+            <div className="box-foot">
+              <span>Product Essence</span>
+              <p>Matured under the bright Himalayan sun and picked when the rays are strong and yet the air remains cool from the misty mountains, these leaves develop a unique depth of flavour known as muscatel.</p>
+            </div>
+          </div>
 
-            <SwiperSlide id="product-slide-3">
-              <span>HIMALAYAN ELIXIR RUBY</span>
-              <img src={productSlide3} alt="Product 3" loading="lazy" />
-              <div className="slide-foot">
-                <span>Product Essence</span>
-                <p>Matured under the bright Himalayan sun, and picked at a time when the sun is strong and the air still cool from the mountains, the leaves develop a unique depth of flavour known as muscatel - a natural fruity note.</p>
-              </div>
-            </SwiperSlide>
+          {/* Box 3 */}
+          <div className="product-box" id="product-box-3">
+            <span className="product-title">HIMALAYAN ELIXIR REGULAR</span>
+            <span className="product-sub-title">Available in Whole Leaf & Fannings/Flakes</span>
+            <div className="product-image">
+              <img src={productImage3} alt="Product Image" loading="lazy" />
+            </div>
+            <div className="box-foot">
+              <span>Product Essence</span>
+              <p>Harvested during the monsoons, it offers a smooth, full-bodied flavor - a daily luxury that brings the mountains to your morning, best enjoyed black or with a dash of milk and sugar. </p>
+            </div>
+          </div>
 
-            <SwiperSlide id="product-slide-4">
-              <span>HIMALAYAN ELIXIR GOLD</span>
-              <img src={productSlide4} alt="Product 4" loading="lazy" />
-              <div className="slide-foot">
-                <span>Product Essence</span>
-                <p>The rarest of rare harvest - plucked at dawn in early spring, when the Himalayan mist is still on the leaves. Lquantity is grown and harvested in entire Darjeeling.</p>
-              </div>
-            </SwiperSlide>
-
-          </Swiper>
-
-          {/* Navigation Buttons */}
-          <div className="swiper-button-next"></div>
-          <div className="swiper-button-prev"></div>
+          {/* Box 4 */}
+          <div className="product-box" id="product-box-4">
+            <span className="product-category" id="retail">RETAIL</span>
+            <div className="product-image">
+              <img src={productImage4} alt="Product Image" loading="lazy" />
+            </div>
+            <div className="box-foot">
+              <span>50 gm | 100 gm</span>
+            </div>
+          </div>
 
         </div>
       </section>
