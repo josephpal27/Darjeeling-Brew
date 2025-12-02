41387a028c8b8f36cd625e9068116bded45f6ab8
Joseph Pal <josephpal27@gmail.com>
2025-12-02 12:50:10 +0530

    About Content Updated

--- src/components/about-content/AboutContent.jsx
+++ src/components/about-content/AboutContent.jsx
@@ -1,6 +1,4 @@
-import { Link } from 'react-router-dom';
 import './AboutContent.css';
-import { BsArrowRightShort } from "react-icons/bs";
 import aboutImage from '../../assets/images/about-content.avif';
 
 const AboutContent = () => {
@@ -8,10 +6,13 @@ const AboutContent = () => {
         <>
             <section className="about-content">
                 <div className="about-content-left">
-                    <h3>The Darjeeling Brew - A Romance with the Himalayas</h3>
+                    <h3>The Darjeeling Story</h3>
+                    <span>Taste the Himalayas. Feel the Romance.</span>
                     <p>In the hush of dawn, when the mists cradle the emerald slopes of Darjeeling, the first rays of sunlight travel across the ridges, touching the tea gardens with gold.</p>
-                    <p>From here, the Himalayas rise like a dream their guardian, Mount Everest, standing eternal in the distance, kissed by clouds and draped in snow.</p>
-                    <Link to="/about-us"><button>Read More <BsArrowRightShort className="icon" /></button></Link>
+                    <p>From here, the Himalayas rise like a dream — their guardian, Mount Everest, standing eternal in the distance, kissed by clouds and draped in snow. It is said that the soul of Darjeeling’s tea is born in the dance 
+                    between earth and sky — in the crisp mountain air that carries whispers from Everest, and in the pure spring waters that trickle down from the high snows to nourish the tender leaves.</p>
+                    <p>Each leaf, plucked by hand, carries the poetry of the land — the laughter of the mist, the sigh of the pines, and the quiet blessing of the world’s highest peak. Legend has it that travellers who sipped this golden liquid felt as if they were drinking the very spirit of the mountains — a spirit that stirred courage, serenity, and an unspoken longing.</p>
+                    <p>The Darjeeling Brew is not merely tea. It is the romance of Everest’s eternal snow with the gardens that sway beneath her gaze.</p>
                 </div>
                 <div className="about-content-right">
                     <img src={aboutImage} alt="About Image" loading="lazy" />
