db7f7591069c0e3bfebdd59d643a1a07b5b24623
Joseph Pal <josephpal27@gmail.com>
2025-12-04 16:57:51 +0530

    Footer Structure updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,32 +11,32 @@ const Footer = () => {
     <>
       <footer>
         <div className="footer-content">
-            <div className="footer-box">
-                <img src={footerLogo} alt="Logo" loading="lazy" />
-                <div className="social-icons">
-                    <a href="#"><FiFacebook className="icon" /></a>
-                    <a href="#"><FaInstagram className="icon" /></a>
-                    <a href="#"><FaXTwitter className="icon" /></a>
-                    <a href="#"><FiYoutube className="icon" /></a>
-                </div>
+          <div className="footer-box">
+            <img src={footerLogo} alt="Logo" loading="lazy" />
+            <div className="social-icons">
+              <a href="#"><FiFacebook className="icon" /></a>
+              <a href="#"><FaInstagram className="icon" /></a>
+              <a href="#"><FaXTwitter className="icon" /></a>
+              <a href="#"><FiYoutube className="icon" /></a>
             </div>
-            <div className="footer-box">
-                <span>Quick Links</span>
-                <Link to="/">Home</Link>
-                <Link to="/origin-of-the-brew">Origin of the Brew</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/darjeeling-story">Darjeeling Story</Link>
-                <Link to="/about-us">About Us</Link>
-                <Link to="/art-of-brewing">Art of Brewing</Link>
-            </div>
-            <div className="footer-box">
-                <span>Contact Info</span>
-                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=wecare@thedarjeelingbrew.com" target="_blank"> <IoMailOpen className="icon"/> wecare@thedarjeelingbrew.com</a>
-                <a href="https://maps.app.goo.gl/m5Gd2rf88xP1sZhu7" target="_blank"> <IoHomeSharp className="icon" /> Kangchenjunga Darjeeling Beverages Pvt. Ltd., Tangra Industrial Estate - II, 45,Radhanath Chowdhury Road, Tangra, Kolkata - 700015</a>
-            </div>
+          </div>
+          <div className="footer-box">
+            <span>Quick Links</span>
+            <Link to="/">Home</Link>
+            <Link to="/origin-of-the-brew">Origin of the Brew</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/darjeeling-story">Darjeeling Story</Link>
+            <Link to="/about-us">About Us</Link>
+            <Link to="/art-of-brewing">Art of Brewing</Link>
+          </div>
+          <div className="footer-box">
+            <span>Contact Info</span>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=wecare@thedarjeelingbrew.com" target="_blank"> <IoMailOpen className="icon" /> wecare@thedarjeelingbrew.com</a>
+            <a href="https://maps.app.goo.gl/m5Gd2rf88xP1sZhu7" target="_blank"> <IoHomeSharp className="icon" /> Kangchenjunga Darjeeling Beverages Pvt. Ltd., Tangra Industrial Estate - II, 45,Radhanath Chowdhury Road, Tangra, Kolkata - 700015</a>
+          </div>
         </div>
         <div className="copyright">
-            <p>Copyright © 2025 | The Darjeeling Brew | All Rights Reserved</p>
+          <p>Copyright © 2025 | The Darjeeling Brew | All Rights Reserved</p>
         </div>
       </footer>
     </>
