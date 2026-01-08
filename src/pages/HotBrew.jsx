0ef3ae78911cc298b7e656f22b8d2297e8e2afb1
Joseph Pal <josephpal27@gmail.com>
2026-01-08 15:55:00 +0530

    Hot Brew Serve added

--- src/pages/HotBrew.jsx
+++ src/pages/HotBrew.jsx
@@ -1,17 +1,30 @@
 import '../css/HotBrew.css';
 
+import BrewServes from '../components/brew-serves/BrewServes';
+
 const HotBrew = () => {
-  return (
-    <>
-      <section className="banner">
-        <img src="/images/banners/hot-brew-banner.avif" alt="Banner" loading="eager" />
-        <div className="banner-content">
-            <h1>SIGNATURE SERVES</h1>
-            <p>A curated collection of modern recipes crafted to highlight the versatility of The Darjeeling Brew</p>
-        </div>
-      </section>
-    </>
-  )
+    return (
+        <>
+            {/* Banner */}
+            <section className="banner">
+                <img src="/images/banners/hot-brew-banner.avif" alt="Banner" loading="eager" />
+                <div className="banner-content">
+                    <h1>SIGNATURE SERVES</h1>
+                    <p>A curated collection of modern recipes crafted to highlight the versatility of The Darjeeling Brew</p>
+                </div>
+            </section>
+
+            {/* Brew Serves */}
+            <section className="brew-serves">
+                <div className="brew-serves-head">
+                    <h2>HOT BREW SERVES</h2>
+                    <p>Comforting, aromatic, and calming</p>
+                </div>
+                {/* Brew Serves Row */}
+                <BrewServes />
+            </section>
+        </>
+    )
 }
 
 export default HotBrew
