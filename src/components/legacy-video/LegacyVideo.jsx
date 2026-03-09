091cbad95ae328783995fa82b9076437edd71fed
Joseph Pal <josephpal27@gmail.com>
2026-03-09 15:11:51 +0530

    Legacy Video Removed

--- src/components/legacy-video/LegacyVideo.jsx
+++ src/components/legacy-video/LegacyVideo.jsx
@@ -7,9 +7,9 @@ const LegacyVideo = () => {
                 <h5 data-aos="fade-up">A Legacy, Steeped in the Hills</h5>
                 <div className="video-box">
                     <div className="video-box-left">
-                        <video autoPlay loop muted playsInline>
+                        {/* <video autoPlay loop muted playsInline>
                             <source src="/videos/darjeeling.mp4" type="video/mp4" />
-                        </video>
+                        </video> */}
                     </div>
                     <div className="video-box-right">
                         <p data-aos="fade">Where nature, nurture, and generations of craftsmanship meet. Every sip is a journey from mountain mist to mindful ritual. Rooted in Darjeeling, crafted for those who value origin and intention.</p>
