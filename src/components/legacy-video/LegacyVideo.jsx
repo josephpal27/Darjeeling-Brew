344466b8c55b5c9bd0613c1f24e187ed81ac0c7c
Joseph Pal <josephpal27@gmail.com>
2026-03-09 15:18:25 +0530

    iframe updated

--- src/components/legacy-video/LegacyVideo.jsx
+++ src/components/legacy-video/LegacyVideo.jsx
@@ -7,7 +7,17 @@ const LegacyVideo = () => {
                 <h5 data-aos="fade-up">A Legacy, Steeped in the Hills</h5>
                 <div className="video-box">
                     <div className="video-box-left">
-                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/TsZ0SjEMM4o?si=9CPJqOcx7elI2_NJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
+                        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/TsZ0SjEMM4o?si=9CPJqOcx7elI2_NJ"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                            loading="lazy"
+                        ></iframe>
                     </div>
                     <div className="video-box-right">
                         <p data-aos="fade">Where nature, nurture, and generations of craftsmanship meet. Every sip is a journey from mountain mist to mindful ritual. Rooted in Darjeeling, crafted for those who value origin and intention.</p>
